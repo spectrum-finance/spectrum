319efd31182647939e64e9acc5b5a1f104895d13
oskin1 <ilya.arcadich@gmail.com>
2022-08-17 20:43:56 +0300

    Hide internals of opaque types.

--- spectrum-network/src/peer/types.rs
+++ spectrum-network/src/peer/types.rs
@@ -2,7 +2,7 @@ use crate::peer::data::ReputationChange;
 
 /// Opaque identifier for an incoming connection. Allocated by the network.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct IncomingIndex(pub u64);
+pub struct IncomingIndex(u64);
 
 impl From<u64> for IncomingIndex {
     fn from(val: u64) -> Self {
@@ -13,7 +13,7 @@ impl From<u64> for IncomingIndex {
 /// Reputation value of the node, between `i32::MIN` (we hate that node) and
 /// `i32::MAX` (we love that node).
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Reputation(pub i32);
+pub struct Reputation(i32);
 
 impl Reputation {
     pub fn initial() -> Self {
