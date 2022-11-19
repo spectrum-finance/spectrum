7fa0667f1796c1d451282539b10309df4efd6182
Tim Ling <791016+kettlebell@users.noreply.github.com>
2022-11-20 00:29:02 +1100

    Refactor sync-handler task
    The task that consumes the sync-handler stream no longer spawns under
    the task that runs the swarm.

--- spectrum-network/tests/integration_tests/mod.rs
+++ spectrum-network/tests/integration_tests/mod.rs
@@ -16,6 +16,7 @@ use spectrum_network::{
         NetworkingConfig, PeerManager, PeerManagerConfig, PeersMailbox,
     },
     protocol::{ProtocolConfig, ProtocolSpec, SYNC_PROTOCOL_ID},
+    protocol_api::ProtocolMailbox,
     protocol_handler::{
         sync::{message::SyncSpec, NodeStatus, SyncBehaviour},
         MalformedMessage, ProtocolBehaviour, ProtocolHandler,
@@ -70,26 +71,32 @@ async fn integration_test_0() {
     let sync_behaviour_1 = |p| SyncBehaviour::new(p, local_status_1);
 
     let (nc_out_tx, mut nc_out_rx) = mpsc::channel(10);
-    let peer_0 = make_swarm_fut(
-        peers_0,
+    let (mut sync_handler_0, nc_0) = make_swarm_components(peers_0, sync_behaviour_0, 10);
+    let (mut sync_handler_1, nc_1) = make_swarm_components(peers_1, sync_behaviour_1, 10);
+
+    let sync_handler_0_handle = async_std::task::spawn(async move {
+        loop {
+            let msg = sync_handler_0.select_next_some().await;
+            dbg!(msg);
+        }
+    });
+
+    let sync_handler_1_handle = async_std::task::spawn(async move {
+        loop {
+            let msg = sync_handler_1.select_next_some().await;
+            dbg!(msg);
+        }
+    });
+
+    let (abortable_peer_0, handle_0) = futures::future::abortable(create_swarm(
         local_key_0,
+        nc_0,
         addr_0,
         Peer::First,
-        sync_behaviour_0,
-        10,
         nc_out_tx.clone(),
-    );
-    let peer_1 = make_swarm_fut(
-        peers_1,
-        local_key_1,
-        addr_1,
-        Peer::Second,
-        sync_behaviour_1,
-        10,
-        nc_out_tx,
-    );
-    let (abortable_peer_0, handle_0) = futures::future::abortable(peer_0);
-    let (abortable_peer_1, handle_1) = futures::future::abortable(peer_1);
+    ));
+    let (abortable_peer_1, handle_1) =
+        futures::future::abortable(create_swarm(local_key_1, nc_1, addr_1, Peer::Second, nc_out_tx));
     let (cancel_tx_0, cancel_rx_0) = oneshot::channel::<()>();
     let (cancel_tx_1, cancel_rx_1) = oneshot::channel::<()>();
 
@@ -97,6 +104,7 @@ async fn integration_test_0() {
     async_std::task::spawn(async move {
         let _ = cancel_rx_0.await;
         handle_0.abort();
+        sync_handler_0_handle.cancel().await;
     });
     async_std::task::spawn(async move {
         wasm_timer::Delay::new(Duration::from_secs(5)).await.unwrap();
@@ -108,6 +116,7 @@ async fn integration_test_0() {
     async_std::task::spawn(async move {
         let _ = cancel_rx_1.await;
         handle_1.abort();
+        sync_handler_1_handle.cancel().await;
     });
     async_std::task::spawn(async move {
         wasm_timer::Delay::new(Duration::from_secs(4)).await.unwrap();
@@ -188,26 +197,32 @@ async fn integration_test_1() {
     let fake_sync_behaviour = |p| FakeSyncBehaviour::new(p, local_status_1);
 
     let (nc_out_tx, mut nc_out_rx) = mpsc::channel(10);
-    let peer_0 = make_swarm_fut(
-        peers_0,
+    let (mut sync_handler_0, nc_0) = make_swarm_components(peers_0, sync_behaviour_0, 10);
+    let (mut sync_handler_1, nc_1) = make_swarm_components(peers_1, fake_sync_behaviour, 10);
+    let sync_handler_0_handle = async_std::task::spawn(async move {
+        loop {
+            let msg = sync_handler_0.select_next_some().await;
+            dbg!(msg);
+        }
+    });
+
+    let sync_handler_1_handle = async_std::task::spawn(async move {
+        loop {
+            let msg = sync_handler_1.select_next_some().await;
+            dbg!(msg);
+        }
+    });
+
+    let (abortable_peer_0, handle_0) = futures::future::abortable(create_swarm(
         local_key_0,
+        nc_0,
         addr_0,
         Peer::First,
-        sync_behaviour_0,
-        10,
         nc_out_tx.clone(),
-    );
-    let peer_1 = make_swarm_fut(
-        peers_1,
-        local_key_1,
-        addr_1,
-        Peer::Second,
-        fake_sync_behaviour,
-        10,
-        nc_out_tx,
-    );
-    let (abortable_peer_0, handle_0) = futures::future::abortable(peer_0);
-    let (abortable_peer_1, handle_1) = futures::future::abortable(peer_1);
+    ));
+    let (abortable_peer_1, handle_1) =
+        futures::future::abortable(create_swarm(local_key_1, nc_1, addr_1, Peer::Second, nc_out_tx));
+
     let (cancel_tx_0, cancel_rx_0) = oneshot::channel::<()>();
     let (cancel_tx_1, cancel_rx_1) = oneshot::channel::<()>();
 
@@ -215,6 +230,7 @@ async fn integration_test_1() {
     async_std::task::spawn(async move {
         let _ = cancel_rx_0.await;
         handle_0.abort();
+        sync_handler_0_handle.cancel().await;
     });
     async_std::task::spawn(async move {
         wasm_timer::Delay::new(Duration::from_secs(6)).await.unwrap();
@@ -226,6 +242,7 @@ async fn integration_test_1() {
     async_std::task::spawn(async move {
         let _ = cancel_rx_1.await;
         handle_1.abort();
+        sync_handler_1_handle.cancel().await;
     });
     async_std::task::spawn(async move {
         wasm_timer::Delay::new(Duration::from_secs(5)).await.unwrap();
@@ -299,29 +316,37 @@ async fn integration_test_peer_punish_too_slow() {
     };
     let local_status_1 = local_status_0.clone();
     let sync_behaviour_0 = |p| SyncBehaviour::new(p, local_status_0);
-    let fake_sync_behaviour = |p| SyncBehaviour::new(p, local_status_1);
+    let sync_behaviour_1 = |p| SyncBehaviour::new(p, local_status_1);
 
     let (nc_out_tx, mut nc_out_rx) = mpsc::channel(10);
-    let peer_0 = make_swarm_fut(
-        peers_0,
+
+    // It's crucial to have a buffer of size 1 for this test
+    let msg_buffer_size = 1;
+    let (mut sync_handler_0, nc_0) = make_swarm_components(peers_0, sync_behaviour_0, msg_buffer_size);
+    let (mut sync_handler_1, nc_1) = make_swarm_components(peers_1, sync_behaviour_1, msg_buffer_size);
+    let sync_handler_0_handle = async_std::task::spawn(async move {
+        loop {
+            let msg = sync_handler_0.select_next_some().await;
+            dbg!(msg);
+        }
+    });
+
+    let sync_handler_1_handle = async_std::task::spawn(async move {
+        loop {
+            let msg = sync_handler_1.select_next_some().await;
+            dbg!(msg);
+        }
+    });
+
+    let (abortable_peer_0, handle_0) = futures::future::abortable(create_swarm(
         local_key_0,
+        nc_0,
         addr_0,
         Peer::First,
-        sync_behaviour_0,
-        1,
         nc_out_tx.clone(),
-    );
-    let peer_1 = make_swarm_fut(
-        peers_1,
-        local_key_1,
-        addr_1,
-        Peer::Second,
-        fake_sync_behaviour,
-        1,
-        nc_out_tx,
-    );
-    let (abortable_peer_0, handle_0) = futures::future::abortable(peer_0);
-    let (abortable_peer_1, handle_1) = futures::future::abortable(peer_1);
+    ));
+    let (abortable_peer_1, handle_1) =
+        futures::future::abortable(create_swarm(local_key_1, nc_1, addr_1, Peer::Second, nc_out_tx));
     let (cancel_tx_0, cancel_rx_0) = oneshot::channel::<()>();
     let (cancel_tx_1, cancel_rx_1) = oneshot::channel::<()>();
 
@@ -329,6 +354,7 @@ async fn integration_test_peer_punish_too_slow() {
     async_std::task::spawn(async move {
         let _ = cancel_rx_0.await;
         handle_0.abort();
+        sync_handler_0_handle.cancel().await;
     });
     async_std::task::spawn(async move {
         wasm_timer::Delay::new(Duration::from_secs(6)).await.unwrap();
@@ -340,6 +366,7 @@ async fn integration_test_peer_punish_too_slow() {
     async_std::task::spawn(async move {
         let _ = cancel_rx_1.await;
         handle_1.abort();
+        sync_handler_1_handle.cancel().await;
     });
     async_std::task::spawn(async move {
         wasm_timer::Delay::new(Duration::from_secs(5)).await.unwrap();
@@ -386,15 +413,15 @@ async fn integration_test_peer_punish_too_slow() {
     );
 }
 
-async fn make_swarm_fut<P, F>(
+fn make_swarm_components<P, F>(
     peers: Vec<PeerDestination>,
-    local_key: identity::Keypair,
-    addr: Multiaddr,
-    peer: Peer,
     gen_protocol_behaviour: F,
     msg_buffer_size: usize,
-    mut tx: mpsc::Sender<(Peer, NetworkControllerOut)>,
-) where
+) -> (
+    ProtocolHandler<P, NetworkMailbox>,
+    NetworkController<PeersMailbox, PeerManager<PeerRepo>, ProtocolMailbox>,
+)
+where
     P: ProtocolBehaviour + Unpin + std::marker::Send + 'static,
     F: FnOnce(PeersMailbox) -> P,
 {
@@ -418,9 +445,7 @@ async fn make_swarm_fut<P, F>(
         prot_alloc_interval: Duration::from_secs(30),
         protocols_allocation: Vec::new(),
     };
-    let local_peer_id = PeerId::from(local_key.public());
-    let transport = libp2p::development_transport(local_key).await.unwrap();
-    let peer_state = PeerRepo::new(netw_config, peers.clone());
+    let peer_state = PeerRepo::new(netw_config, peers);
     let (peer_manager, peers) = PeerManager::new(peer_state, peer_manager_conf);
     let sync_conf = ProtocolConfig {
         supported_versions: vec![(
@@ -436,7 +461,7 @@ async fn make_swarm_fut<P, F>(
     let network_api = NetworkMailbox {
         mailbox_snd: requests_snd,
     };
-    let (mut sync_handler, sync_mailbox) =
+    let (sync_handler, sync_mailbox) =
         ProtocolHandler::new(gen_protocol_behaviour(peers.clone()), network_api);
     let nc = NetworkController::new(
         peer_conn_handler_conf,
@@ -446,13 +471,19 @@ async fn make_swarm_fut<P, F>(
         requests_recv,
     );
 
+    (sync_handler, nc)
+}
+
+async fn create_swarm(
+    local_key: identity::Keypair,
+    nc: NetworkController<PeersMailbox, PeerManager<PeerRepo>, ProtocolMailbox>,
+    addr: Multiaddr,
+    peer: Peer,
+    mut tx: mpsc::Sender<(Peer, NetworkControllerOut)>,
+) {
+    let transport = libp2p::development_transport(local_key.clone()).await.unwrap();
+    let local_peer_id = PeerId::from(local_key.public());
     let mut swarm = Swarm::new(transport, nc, local_peer_id);
-    async_std::task::spawn(async move {
-        loop {
-            let msg = sync_handler.select_next_some().await;
-            dbg!(msg);
-        }
-    });
 
     swarm.listen_on(addr).unwrap();
 
