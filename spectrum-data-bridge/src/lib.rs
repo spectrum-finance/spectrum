0cba6a4fe309383b6c9291c1c58c9852aeb55168
Tim Ling <791016+kettlebell@users.noreply.github.com>
2023-04-09 16:53:37 +1000

    Impl `ErgoDataBridge`

---
+++ spectrum-data-bridge/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod data_bridge;
+
+#[derive(Clone, Debug)]
+pub enum TxEvent<T> {
+    AppliedTx(T),
+    UnappliedTx(T),
+}
+
+pub trait DataBridge {
+    type TxType;
+    fn get_components(self) -> DataBridgeComponents<Self::TxType>;
+}
+
+pub struct DataBridgeComponents<T> {
+    /// Each consumer of the data bridge is given a receiver to stream transaction data.
+    pub receivers: Vec<tokio::sync::broadcast::Receiver<TxEvent<T>>>,
+    /// Call `send(())` on this `Sender` to indicate that the bridge should start transmitting
+    /// transaction data. Note that the receivers should have already been distributed to
+    /// consumers.
+    pub start_signal: tokio::sync::oneshot::Sender<()>,
+}
