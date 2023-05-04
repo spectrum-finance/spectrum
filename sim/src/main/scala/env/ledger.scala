755cce6ad4ff5a98aa2aa3bb3b8fab850f610208
aleksandroranov <alexandr.oranov@gmail.com>
2023-05-04 14:25:31 +0300

    Register message added.

--- sim/src/main/scala/env/ledger.scala
+++ sim/src/main/scala/env/ledger.scala
@@ -1,12 +1,27 @@
 package sim.env
+import cats.effect.{ExitCode, IO, IOApp}
 
-case class Account(value: String)
+case class Address(v: String)
+
+case class VrfPublicKey(value: String)  // HexString
+case class KesPublicKey(value: String)  // HexString
+case class DsigPublicKey(value: String) // HexString
+
+case class Account(
+    address: Address,
+    stake: Long,
+    vrfPk: VrfPublicKey,
+    kesPk: KesPublicKey,
+    dsigPk: DsigPublicKey
+)
 
 case class Transaction(id: Long, account: Account, change: Long)
 
 case class BlockId(value: Long)
 
-case class Block(id: BlockId, txs: Vector[Transaction], seqNum: Int)
+case class Block(id: BlockId, seqNum: Int)
+
+case class State(verAccounts: Vector[Account]) // set of verification keys to store in the Ledger
 
 trait Ledger[F[_]]:
   /** Add a new block to the ledger.
@@ -20,3 +35,5 @@ trait Ledger[F[_]]:
   /** Get specific block.
     */
   def get(id: BlockId): F[Option[Block]]
+  
+  def addParticipant(acc: Account): F[Unit]
--- sim/src/main/scala/protocol/spectrum.scala
+++ sim/src/main/scala/protocol/spectrum.scala
@@ -1,6 +1,8 @@
 package sim.spectrum
 
 import cats.Monad
+import cats.effect.IO
+import cats.effect.kernel.{Async, Clock}
 import cats.effect.std.{AtomicCell, Queue}
 import cats.syntax.applicative.*
 import cats.syntax.flatMap.*
@@ -10,23 +12,30 @@ import cats.syntax.traverse.*
 import sim.*
 import sim.env.*
 
+import scala.concurrent.duration.*
+import scala.language.postfixOps
+
+
 sealed trait SpectrumMsg
 case class NewBlock(block: Block) extends SpectrumMsg
-case class Inv(blockId: BlockId)  extends SpectrumMsg
+case class GetBlock(blockId: BlockId)  extends SpectrumMsg
+case class Register(newAcc: Account)  extends SpectrumMsg
+
 
 final class Spectrum[F[_]: Monad](
-    selfAddr: Addr,
-    // abstract interface to Ledger, so you dont have to care about impl.
-    ledger: Ledger[F],
-    // queue of pending output commands waiting to be returned from poll().
-    pendingOutputs: Queue[F, (Addr, AgentOut[SpectrumMsg])]
-) extends Agent[SpectrumMsg, F]:
+                                   selfAddr: Addr,
+                                   // abstract interface to Ledger, so you dont have to care about impl.
+                                   ledger: Ledger[F],
+                                   // queue of pending output commands waiting to be returned from poll().
+                                   pendingOutputs: Queue[F, (Addr, AgentOut[SpectrumMsg])]
+                                 ) extends Agent[SpectrumMsg, F]:
   def getAddr: F[Addr] = selfAddr.pure
 
   def injectMessage(srcAddr: Addr, m: SpectrumMsg): F[Unit] =
     m match
       case NewBlock(block) => ledger.add(block)
-      case Inv(blockId) =>
+      case Register(account) => ledger.addParticipant(account)
+      case GetBlock(blockId) =>
         for
           maybeBlock <- ledger.get(blockId)
           _ <- maybeBlock match
@@ -35,7 +44,7 @@ final class Spectrum[F[_]: Monad](
               ().pure
             case Some(block) =>
               // if the requested block is found locally we send it to the peer which requested it
-              pendingOutputs.offer(selfAddr -> SendMessage(NewBlock(block)))
+              pendingOutputs.offer(srcAddr -> SendMessage(NewBlock(block)))
         yield ()
 
   def injectConnReq(peerAddr: Addr, handshake: SpectrumMsg): F[Either[Reject, SpectrumMsg]] = ???
