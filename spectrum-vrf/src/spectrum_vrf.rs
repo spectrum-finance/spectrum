5b745ba6cc9084c3056315a58767b3e189bcb9e4
aleksandroranov <alexandr.oranov@gmail.com>
2023-05-26 16:40:46 +0300

    Fixed types.

--- spectrum-vrf/src/spectrum_vrf.rs
+++ spectrum-vrf/src/spectrum_vrf.rs
@@ -3,25 +3,35 @@ use crate::{AffinePoint, PublicKey, Scalar, Secp256k1, SecretKey, Sha2Digest256,
 
 pub struct SpectrumVRF;
 
-impl ECVRF<TCurve> for SpectrumVRF {
+impl ECVRF<Secp256k1> for SpectrumVRF {
     type Error = ();
 
     fn gen(&self)
-           -> Result<(SecretKey<TCurve>, PublicKey<TCurve>), Self::Error> {
-        // todo!()
+           -> Result<(SecretKey<Secp256k1>, PublicKey<Secp256k1>), Self::Error> {
+        todo!()
     }
 
-    fn prove(&self, sk: SecretKey<TCurve>, message_hash: Sha2Digest256)
-             -> Result<VRFProof<TCurve>, Self::Error> {
-        // todo!()
+    fn prove(&self, sk: SecretKey<Secp256k1>, message_hash: Sha2Digest256)
+             -> Result<ECVRFProof<Secp256k1>, Self::Error> {
+        todo!()
     }
 
     fn verify(
         &self,
-        pk: PublicKey<TCurve>,
-        message_hash: &[u8],
-        proof: VRFProof<TCurve>,
-    ) -> Result<AffinePoint<TCurve>, Self::Error> {
-        // todo!()
+        pk: PublicKey<Secp256k1>,
+        message_hash: Sha2Digest256,
+        proof: ECVRFProof<Secp256k1>,
+    ) -> Result<AffinePoint<Secp256k1>, Self::Error> {
+        todo!()
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_key_gen() {
+//         let s = "769a48bdb72d0f7bade76a120982b6d479fda6084c84d40957ae9f935f3b99ac";
+//     }
+// }
