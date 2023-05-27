429f5adef1507cab4a4847edc2b9d6d79ad43bac
aleksandroranov <alexandr.oranov@gmail.com>
2023-05-27 09:50:02 +0300

    TCurve generic.

--- spectrum-vrf/src/spectrum_vrf.rs
+++ spectrum-vrf/src/spectrum_vrf.rs
@@ -1,37 +1,34 @@
-use crate::{AffinePoint, PublicKey, Scalar, Secp256k1, SecretKey, Sha2Digest256, ECVRFProof, ECVRF};
+use elliptic_curve::CurveArithmetic;
+use elliptic_curve::rand_core::OsRng;
 
+use crate::{AffinePoint, ECVRF, ECVRFProof, PublicKey, Secp256k1,
+            SecretKey, Sha2Digest256};
 
-pub struct SpectrumVRF;
+pub struct SpectrumVRF<TCurve>(TCurve);
 
-impl ECVRF<Secp256k1> for SpectrumVRF {
+impl<TCurve> ECVRF<TCurve> for SpectrumVRF<TCurve>
+    where TCurve: CurveArithmetic {
     type Error = ();
 
     fn gen(&self)
-           -> Result<(SecretKey<Secp256k1>, PublicKey<Secp256k1>), Self::Error> {
-        todo!()
+           -> Result<(SecretKey<TCurve>, PublicKey<TCurve>), Self::Error> {
+        let spectrum_vrf_sk = SecretKey::<TCurve>::random(&mut OsRng);
+        let spectrum_vrf_pk = PublicKey::from_secret_scalar(
+            &spectrum_vrf_sk.to_nonzero_scalar());
+        Ok((spectrum_vrf_sk, spectrum_vrf_pk))
     }
 
-    fn prove(&self, sk: SecretKey<Secp256k1>, message_hash: Sha2Digest256)
-             -> Result<ECVRFProof<Secp256k1>, Self::Error> {
+    fn prove(&self, sk: SecretKey<TCurve>, message_hash: Sha2Digest256)
+             -> Result<ECVRFProof<TCurve>, Self::Error> {
         todo!()
     }
 
     fn verify(
         &self,
-        pk: PublicKey<Secp256k1>,
+        pk: PublicKey<TCurve>,
         message_hash: Sha2Digest256,
-        proof: ECVRFProof<Secp256k1>,
-    ) -> Result<AffinePoint<Secp256k1>, Self::Error> {
+        proof: ECVRFProof<TCurve>,
+    ) -> Result<AffinePoint<TCurve>, Self::Error> {
         todo!()
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn test_key_gen() {
-//         let s = "769a48bdb72d0f7bade76a120982b6d479fda6084c84d40957ae9f935f3b99ac";
-//     }
-// }
