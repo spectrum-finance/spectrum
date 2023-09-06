b0c7e3666fb34a62212d07fcb646cec4c815a0cb
Tim Ling <791016+kettlebell@users.noreply.github.com>
2023-09-06 15:16:55 +1000

    Documented code for verifying schnorr signatures

--- spectrum-ergo-connector/src/script.rs
+++ spectrum-ergo-connector/src/script.rs
@@ -12,6 +12,8 @@ mod tests {
     use std::collections::HashMap;
     use std::io::Write;
 
+    use base64::prelude::BASE64_STANDARD_NO_PAD;
+    use base64::Engine;
     use bytes::Bytes;
     use elliptic_curve::ops::LinearCombination;
     use elliptic_curve::ops::Reduce;
@@ -184,7 +186,6 @@ mod tests {
             UnsignedTransaction::new(TxIoVec::from_vec(vec![unsigned_input]).unwrap(), None, outputs)
                 .unwrap();
         let tx_context = TransactionContext::new(unsigned_tx, vec![input_box], vec![]).unwrap();
-        const SEED_PHRASE: &str = "gather gather gather gather gather gather gather gather gather gather gather gather gather gather gather";
         let wallet = get_wallet();
         let ergo_state_context: ErgoStateContext = dummy_ergo_state_context();
         let res = wallet.sign_transaction(tx_context, &ergo_state_context, None);
@@ -194,8 +195,8 @@ mod tests {
 
     #[test]
     fn test_verify_schnorr_signature() {
-        // Script: https://wallet.plutomonkey.com/p2s/?source=ewogIHZhbCBwdWJLZXkgICAgICAgICA9IElOUFVUUygwKS5SNFtHcm91cEVsZW1lbnRdLmdldAogIHZhbCBjaGFsbGVuZ2UgICAgICA9IElOUFVUUygwKS5SNVsoSW50LCBDb2xsW0J5dGVdKV0uZ2V0CiAgdmFsIHJlc3BvbnNlICAgICAgID0gSU5QVVRTKDApLlI2W0dyb3VwRWxlbWVudF0uZ2V0IC8vIFIgaW4gQklQLTAzNDAKICB2YWwgbWVzc2FnZSAgICAgICAgPSBJTlBVVFMoMCkuUjdbQ29sbFtCeXRlXV0uZ2V0CiAgdmFsIGdyb3VwR2VuZXJhdG9yID0gSU5QVVRTKDApLlI4W0dyb3VwRWxlbWVudF0uZ2V0CiAgdmFsIHAxT3ZlcjIgICAgICAgID0gSU5QVVRTKDApLlI5W0JpZ0ludF0uZ2V0CgogIHZhbCByciAgICAgICAgPSBnZXRWYXJbQ29sbFtCeXRlXV0oMCkuZ2V0CiAgdmFsIHJpZ2h0ICAgICA9IGdldFZhcltHcm91cEVsZW1lbnRdKDEpLmdldAogIHZhbCBwa0J5dGVzICAgPSBnZXRWYXJbQ29sbFtCeXRlXV0oMikuZ2V0IC8vIGVuY29kZWQgeC1jb29yZGluYXRlIG9mIFAKCiAgZGVmIG15RXhwKGU6IChHcm91cEVsZW1lbnQsIChDb2xsW0J5dGVdLCBJbnQpKSkgOiBHcm91cEVsZW1lbnQgPSB7CiAgICB2YWwgeCA9IGUuXzEKICAgIHZhbCB5ID0gZS5fMi5fMQogICAgdmFsIGxlbiA9IGUuXzIuXzIKICAgIHZhbCB1cHBlciA9IGJ5dGVBcnJheVRvQmlnSW50KHkuc2xpY2UoMCwgbGVuKSkKICAgIHZhbCBsb3dlciA9IGJ5dGVBcnJheVRvQmlnSW50KHkuc2xpY2UobGVuLCB5LnNpemUpKQogICAKICAgIHguZXhwKHVwcGVyKS5leHAocDFPdmVyMikubXVsdGlwbHkoeC5leHAobG93ZXIpKQogIH0KCiAgZGVmIHRvU2lnbmVkQnl0ZXMoYjogQ29sbFtCeXRlXSkgOiBDb2xsW0J5dGVdID0gewogICAgaWYgKGIoMCkgPCAwICkgeyAvLyYmICEoYigwKSA9PSAxMjggJiYgYi5zbGljZSgxLGIuc2l6ZSkuZm9yYWxsIHsoYjogQnl0ZSkgPT4gYiA9PSAwfSkpIHsKICAgICAgICBDb2xsKDAudG9CeXRlKS5hcHBlbmQoYikKICAgIH0gZWxzZSB7CiAgICAgICAgYgogICAgfQogIH0KCiAgZGVmIGdldEZpYXRTaGFtaXJDaGFsbGVuZ2UoZSA6IChHcm91cEVsZW1lbnQsIChHcm91cEVsZW1lbnQsIENvbGxbQnl0ZV0pKSk6IChJbnQsIENvbGxbQnl0ZV0pID0gewogICAgdmFsIHBrID0gZS5fMQogICAgdmFsIGNvbW1pdG1lbnQgPSBlLl8yLl8xCiAgICB2YWwgbXNnID0gZS5fMi5fMgogCiAgICAvLyBCSVAtMDM0MCB1c2VzIHNvLWNhbGxlZCB0YWdnZWQgaGFzaGVzCiAgICB2YWwgY2hhbGxlbmdlVGFnID0gc2hhMjU2KENvbGwoNjYsIDczLCA4MCwgNDgsIDUxLCA1MiwgNDgsIDQ3LCA5OSwgMTA0LCA5NywgMTA4LCAxMDgsIDEwMSwgMTEwLCAxMDMsIDEwMSkubWFwIHsgKHg6SW50KSA9PiB4LnRvQnl0ZSB9KQogICAgdmFsIHJhdyA9IHNoYTI1NihjaGFsbGVuZ2VUYWcgKysgY2hhbGxlbmdlVGFnICsrIHJyICsrIHBrQnl0ZXMgKysgbXNnKQogICAgdmFsIGZpcnN0ID0gdG9TaWduZWRCeXRlcyhyYXcuc2xpY2UoMCwxNikpCiAgICAoZmlyc3Quc2l6ZSwgZmlyc3QuYXBwZW5kKHRvU2lnbmVkQnl0ZXMocmF3LnNsaWNlKDE2LHJhdy5zaXplKSkpKQogICAgCiAgfQogIAogIGRlZiB2ZXJpZnlTY2hub3JyKGU6IChHcm91cEVsZW1lbnQsIChDb2xsW0J5dGVdLCAoR3JvdXBFbGVtZW50LCBDb2xsW0J5dGVdKSkpKTogQm9vbGVhbiA9IHsKICAgIHZhbCBwayAgICAgICAgID0gZS5fMQogICAgdmFsIHMgICAgICAgICAgPSBlLl8yLl8xCiAgICB2YWwgciAgICAgICAgICA9IGUuXzIuXzIuXzEKICAgIHZhbCBtc2cgICAgICAgID0gZS5fMi5fMi5fMgogICAgdmFsIGNoYWxsICA9IGdldEZpYXRTaGFtaXJDaGFsbGVuZ2UoKHBrLCAociwgbXNnKSkpCgogICAgbXlFeHAoKGdyb3VwR2VuZXJhdG9yLCAocywgY2hhbGxlbmdlLl8xKSkpID09ICBteUV4cCgocGssIChjaGFsbC5fMiwgY2hhbGwuXzEpKSkubXVsdGlwbHkocikKICB9CgogIHNpZ21hUHJvcCAodmVyaWZ5U2Nobm9ycigocHViS2V5LCAoY2hhbGxlbmdlLl8yLCAocmVzcG9uc2UsIG1lc3NhZ2UpKSkpKQp9
-        const SCRIPT_BYTES: &str = "W56sEkkPGgLFS4QvRKj2uLg28yQPWP5x7oG6pJtwPApV9yGiN9yEpYFEvnZCELrMvNNjGQE9qm5HBiJ3FgTMC7Tw9p1D3bp32RRu6HpzGJ7bxHyyMKhhY8N7Wo6ZNTSnohxX1ghiTjh3y4moBmzHjE5scRACJG1mjEKDmegbHMeryFyzkAA8GCqT1M2nzgbebrVqLPFsSMqy34u3i9D4WRxUpZdczHuNhqq4KSzHKoXKCTVFHJfLy2AsygzXs83fJ1wjKjtDCZC84yGVivd2u8fzE6dqEdmQqo1hivSibywZyLUqqAyS2eV5wp3d8AgBoDSCAoEEYW9qXk5LBU7hQcQm7qj8rUywMm58pve3hdctPNzp6GrxYJhYDVyfQr4Sg7kM9ZgZ4MwvEwAg126bkWt43QgpgGGb7SGDznTsFFDh3ovEBugLx2S6fUa2vE9Zc7QpavqjTztJDfLLuhZvHSDxKj6xsRWCRosidHxb53DcpEKCGFFuZPuRAAQN7iaj5ur9tS1Xa4r1cQ5PwBCxJj7Giwqm8E8Ct6EnNPKa95R1wdD";
+        // Script: https://wallet.plutomonkey.com/p2s/?source=ewogIHZhbCBwdWJLZXlUdXBsZSAgICA9IElOUFVUUygwKS5SNFsoR3JvdXBFbGVtZW50LCBDb2xsW0J5dGVdKV0uZ2V0CiAgdmFsIHMgICAgICAgICAgICAgID0gSU5QVVRTKDApLlI1WyhDb2xsW0J5dGVdLCBJbnQpXS5nZXQKICB2YWwgcmVzcG9uc2VUdXBsZSAgPSBJTlBVVFMoMCkuUjZbKEdyb3VwRWxlbWVudCwgQ29sbFtCeXRlXSldLmdldCAKICB2YWwgbWVzc2FnZSAgICAgICAgPSBJTlBVVFMoMCkuUjdbQ29sbFtCeXRlXV0uZ2V0CiAgdmFsIGdyb3VwR2VuZXJhdG9yID0gSU5QVVRTKDApLlI4W0dyb3VwRWxlbWVudF0uZ2V0CgogIHZhbCBwdWJLZXkgICAgICAgICA9IHB1YktleVR1cGxlLl8xIC8vIFAKICB2YWwgcGtCeXRlcyAgICAgICAgPSBwdWJLZXlUdXBsZS5fMiAvLyBlbmNvZGVkIHgtY29vcmRpbmF0ZSBvZiBQCiAgdmFsIHJlc3BvbnNlICAgICAgID0gcmVzcG9uc2VUdXBsZS5fMSAvLyBSIGluIEJJUC0wMzQwCiAgdmFsIHJCeXRlcyAgICAgICAgID0gcmVzcG9uc2VUdXBsZS5fMiAvLyBCeXRlIHJlcHJlc2VudGF0aW9uIG9mICdyJwoKICAKICAvLyBQZXJmb3JtcyBleHBvbmVudGlhdGlvbiBvZiBhIEdyb3VwRWxlbWVudCBieSBhbiB1bnNpZ25lZCAyNTZiaXQKICAvLyBpbnRlZ2VyIEkgdXNpbmcgdGhlIGZvbGxvd2luZyBkZWNvbXBvc2l0aW9uIG9mIEk6CiAgLy8gTGV0IGUgPSAoZywgKGIsIG4pKS4gVGhlbiB0aGlzIGZ1bmN0aW9uIGNvbXB1dGVzOgogIC8vCiAgLy8gICBnXkkgPT0gKGdeYigwLG4pKV5wICogZ14oYihuLi4pKQogIC8vIHdoZXJlCiAgLy8gIC0gYigwLG4pIGlzIHRoZSBmaXJzdCBuIGJ5dGVzIG9mIGEgcG9zaXRpdmUgQmlnSW50IGBVYAogIC8vICAtIGIobi4uKSBhcmUgdGhlIHJlbWFpbmluZyBieXRlcyBzdGFydGluZyBmcm9tIGluZGV4IG4uIFRoZXNlIGJ5dGVzCiAgLy8gICAgYWxzbyByZXByZXNlbnQgYSBwb3NpdGl2ZSBCaWdJbnQgYExgLgogIC8vICAtIHAgaXMgMzQwMjgyMzY2OTIwOTM4NDYzNDYzMzc0NjA3NDMxNzY4MjExNDU2IGJhc2UgMTAuCiAgLy8gIC0gSSA9PSBVICogcCArIEwKICBkZWYgbXlFeHAoZTogKEdyb3VwRWxlbWVudCwgKENvbGxbQnl0ZV0sIEludCkpKSA6IEdyb3VwRWxlbWVudCA9IHsKICAgIHZhbCB4ID0gZS5fMQogICAgdmFsIHkgPSBlLl8yLl8xCiAgICB2YWwgbGVuID0gZS5fMi5fMgogICAgdmFsIHVwcGVyID0gYnl0ZUFycmF5VG9CaWdJbnQoeS5zbGljZSgwLCBsZW4pKQogICAgdmFsIGxvd2VyID0gYnl0ZUFycmF5VG9CaWdJbnQoeS5zbGljZShsZW4sIHkuc2l6ZSkpCgogICAgLy8gVGhlIGZvbGxvd2luZyB2YWx1ZSBpcyAzNDAyODIzNjY5MjA5Mzg0NjM0NjMzNzQ2MDc0MzE3NjgyMTE0NTYgYmFzZS0xMC4KICAgIHZhbCBwID0gYnl0ZUFycmF5VG9CaWdJbnQoZnJvbUJhc2U2NCgiQVFBQUFBQUFBQUFBQUFBQUFBQUFBQUEiKSkKICAgCiAgICB4LmV4cCh1cHBlcikuZXhwKHApLm11bHRpcGx5KHguZXhwKGxvd2VyKSkKICB9CgogIC8vIENvbnZlcnRzIGEgYmlnLWVuZGlhbiBieXRlIHJlcHJlc2VudGF0aW9uIG9mIGFuIHVuc2lnbmVkIGludGVnZXIgaW50byBpdHMKICAvLyBlcXVpdmFsZW50IHNpZ25lZCByZXByZXNlbnRhdGlvbgogIGRlZiB0b1NpZ25lZEJ5dGVzKGI6IENvbGxbQnl0ZV0pIDogQ29sbFtCeXRlXSA9IHsKICAgIC8vIE5vdGUgdGhhdCBhbGwgaW50ZWdlcnMgKGluY2x1ZGluZyBCeXRlKSBpbiBFcmdvc2NyaXB0IGFyZSBzaWduZWQuIEluIHN1Y2gKICAgIC8vIGEgcmVwcmVzZW50YXRpb24sIHRoZSBtb3N0LXNpZ25pZmljYW50IGJpdCAoTVNCKSBpcyB1c2VkIHRvIHJlcHJlc2VudCB0aGUKICAgIC8vIHNpZ247IDAgZm9yIGEgcG9zaXRpdmUgaW50ZWdlciBhbmQgMSBmb3IgbmVnYXRpdmUuIE5vdyBzaW5jZSBgYmAgaXMgYmlnLQogICAgLy8gZW5kaWFuLCB0aGUgTVNCIHJlc2lkZXMgaW4gdGhlIGZpcnN0IGJ5dGUgYW5kIE1TQiA9PSAxIGluZGljYXRlcyB0aGF0IGV2ZXJ5CiAgICAvLyBiaXQgaXMgdXNlZCB0byBzcGVjaWZ5IHRoZSBtYWduaXR1ZGUgb2YgdGhlIGludGVnZXIuIFRoaXMgbWVhbnMgdGhhdCBhbgogICAgLy8gZXh0cmEgMC1iaXQgbXVzdCBiZSBwcmVwZW5kZWQgdG8gYGJgIHRvIHJlbmRlciBpdCBhIHZhbGlkIHBvc2l0aXZlIHNpZ25lZAogICAgLy8gaW50ZWdlci4KICAgIC8vCiAgICAvLyBOb3cgc2lnbmVkIGludGVnZXJzIGFyZSBuZWdhdGl2ZSBpZmYgTVNCID09IDEsIGhlbmNlIHRoZSBjb25kaXRpb24gYmVsb3cuCiAgICBpZiAoYigwKSA8IDAgKSB7CiAgICAgICAgQ29sbCgwLnRvQnl0ZSkuYXBwZW5kKGIpCiAgICB9IGVsc2UgewogICAgICAgIGIKICAgIH0KICB9CiAgCiAgICAKICAvLyBCSVAtMDM0MCB1c2VzIHNvLWNhbGxlZCB0YWdnZWQgaGFzaGVzCiAgdmFsIGNoYWxsZW5nZVRhZyA9IHNoYTI1NihDb2xsKDY2LCA3MywgODAsIDQ4LCA1MSwgNTIsIDQ4LCA0NywgOTksIDEwNCwgOTcsIDEwOCwgMTA4LCAxMDEsIDExMCwgMTAzLCAxMDEpLm1hcCB7ICh4OkludCkgPT4geC50b0J5dGUgfSkKICB2YWwgcmF3ID0gc2hhMjU2KGNoYWxsZW5nZVRhZyArKyBjaGFsbGVuZ2VUYWcgKysgckJ5dGVzICsrIHBrQnl0ZXMgKysgbWVzc2FnZSkKCiAgLy8gTm90ZSB0aGF0IHRoZSBvdXRwdXQgb2YgU0hBMjU2IGlzIGEgY29sbGVjdGlvbiBvZiBieXRlcyB0aGF0IHJlcHJlc2VudHMgYW4gdW5zaWduZWQgMjU2Yml0IGludGVnZXIuIAogIHZhbCBmaXJzdCA9IHRvU2lnbmVkQnl0ZXMocmF3LnNsaWNlKDAsMTYpKQogIHZhbCBjb25jYXRCeXRlcyA9IGZpcnN0LmFwcGVuZCh0b1NpZ25lZEJ5dGVzKHJhdy5zbGljZSgxNixyYXcuc2l6ZSkpKQogIHZhbCBmaXJzdEludE51bUJ5dGVzID0gZmlyc3Quc2l6ZQoKICBzaWdtYVByb3AgKG15RXhwKChncm91cEdlbmVyYXRvciwgcykpID09ICBteUV4cCgocHViS2V5LCAoY29uY2F0Qnl0ZXMsIGZpcnN0SW50TnVtQnl0ZXMpKSkubXVsdGlwbHkocmVzcG9uc2UpKQp9
+        const SCRIPT_BYTES: &str = "2uepSNTcPtZBDLuYC1AwzuuBjbHd1WZo9gcn4vWaaaA6iuyRyeAvQsH5YrmuWvcqdG96tq5v1r9nzuKSWFW5voYahwMEyGxfi3A5L9Rozg2QLPJWEH3AsuhKj1HjmZ8g1GH3uAXtrTmdvwJ65A1LjKYSts6S2QiZxt1nCRtg3FdmTzkZsYsDn2QHmHgU38mfR4B6j7QAN2zZn6ijMiteyySrFH6o9ANPzcxVWr6ZCcq1W2oytV88eomwcYVQuqvPuoxv5db8LwhmZ3gteWQQvEdFgiz4S3fGFf6tofEc6WkySVnFAfdFqoo8pgCE4YREpuwvkt6jrEL1up37C9MfGifyBdxfCzgKanYe1unqSc1Mr1KQG9CS5D4RLpqspUYDGw3guhMirKMe2B5ypLvEu75DabhP25nT6eKJYX5E5bFaTckaWxEL6frhBuHQ74VHNYJswtqZWjeVB12QYzEytX9Vi3P";
 
         let encoder = AddressEncoder::new(NetworkPrefix::Mainnet);
         let address = encoder.parse_address_from_str(SCRIPT_BYTES).unwrap();
@@ -205,15 +206,17 @@ mod tests {
         let msg = b"foo".as_slice();
         let mut rng = OsRng;
         let secret_key = SecretKey::random(&mut rng);
-        let pk = secret_key.public_key().to_projective();
         let signing_key = SigningKey::from(secret_key);
         let signature = signing_key.sign(msg);
         let signature_bytes = signature.to_bytes();
 
+        // The components (r,s) of the taproot `Signature` struct are not public, but we can
+        // extract it through its byte representation.
         let (r_bytes, s_bytes) = signature_bytes.split_at(32);
         let r: FieldElement = Option::from(FieldElement::from_bytes(r_bytes.into())).unwrap();
 
         const CHALLENGE_TAG: &[u8] = b"BIP0340/challenge";
+        //  int(sha256(sha256(CHALLENGE_TAG) || sha256(CHALLENGE_TAG) || bytes(r) || bytes(P) || m)) mod n
         let e = <Scalar as Reduce<U256>>::reduce_bytes(
             &tagged_hash(CHALLENGE_TAG)
                 .chain_update(r.to_bytes())
@@ -222,6 +225,8 @@ mod tests {
                 .finalize(),
         );
         let s = NonZeroScalar::try_from(s_bytes).unwrap();
+
+        // R
         let r_point = ProjectivePoint::lincomb(
             &ProjectivePoint::GENERATOR,
             &s,
@@ -229,8 +234,29 @@ mod tests {
             &-e,
         );
 
-        let right = r_point + ProjectivePoint::from(signing_key.verifying_key().as_affine()) * e;
-
+        // The taproot signature satisfies:
+        //     g ^ s == R * P^e
+        // Note: `k256` uses additive notation for elliptic-curves, so we can compute the right
+        // hand side with:
+        //   r_point + ProjectivePoint::from(signing_key.verifying_key().as_affine()) * e;
+        //
+        // Note in the above equation that the values `s` and `e` have a 256bit UNSIGNED integer
+        // representation. This is a problem for Ergoscript since the largest integer values it
+        // allows for is 256bit signed. We can work around the problem by splitting the value
+        // into 2 signed ints.
+        //
+        // Let `B` denote the big-endian unsigned byte representation of `s`. Let `U` and `L`
+        // denote the first 16 and last 16 bytes of `B`, respectively. Then `U` and `L` are
+        // themselves unsigned integers. Moreover,
+        //    B == U*p + L, where p == 340282366920938463463374607431768211456
+        //
+        // We want to use this decomposition on the ergo side, but we need to convert `U` and `L`
+        // into signed integers, `U_S` and `L_S`. We need to be careful as `U_S` and/or `L_S` could
+        // each require 17 bytes if the most-significant-bit of `U`/`L` is 1 (and so we need to
+        // prepend a zero byte to accomodate the sign-bit).
+        //
+        // So we can transport `s` across the boundary with the bytes of [U_S | L_S], and decoding
+        // `U_S` and `L_S` within Ergoscript.
         let s_biguint = scalar_to_biguint(*s.as_ref());
         println!("# bytes: {}", s_biguint.to_bytes_be().len());
         let upper = BigUint::from_bytes_be(&s_biguint.to_bytes_be()[..16]);
@@ -246,21 +272,34 @@ mod tests {
         s_bytes.extend(lower_256.to_signed_bytes_be());
 
         println!("first_len: {}, S_BYTES_LEN: {}", first_len, s_bytes.len());
-        let pp = BigInt256::from_str_radix("340282366920938463463374607431768211456", 10).unwrap();
+        let p = BigInt256::from_str_radix("340282366920938463463374607431768211456", 10).unwrap();
+
+        println!(
+            "PP_base64: {}",
+            base64::engine::general_purpose::STANDARD_NO_PAD.encode(p.to_signed_bytes_be())
+        );
 
         let mut registers = HashMap::new();
 
+        // P from BIP-0340
+        let pubkey_point = EcPoint::from(ProjectivePoint::from(signing_key.verifying_key().as_affine()));
+        // The x-coordinate of P
+        let pubkey_x_coords = signing_key.verifying_key().to_bytes().to_vec();
         registers.insert(
             NonMandatoryRegisterId::R4,
-            Constant::from(EcPoint::from(ProjectivePoint::from(
-                signing_key.verifying_key().as_affine(),
-            ))),
+            (Constant::from(pubkey_point), Constant::from(pubkey_x_coords)).into(),
         );
-        registers.insert(NonMandatoryRegisterId::R5, (first_len, s_bytes).into());
-        registers.insert(NonMandatoryRegisterId::R6, Constant::from(EcPoint::from(r_point)));
+        registers.insert(NonMandatoryRegisterId::R5, (s_bytes, first_len).into());
+        registers.insert(
+            NonMandatoryRegisterId::R6,
+            (
+                Constant::from(EcPoint::from(r_point)),
+                Constant::from(r.to_bytes().to_vec()),
+            )
+                .into(),
+        );
         registers.insert(NonMandatoryRegisterId::R7, Constant::from(msg.to_vec()));
         registers.insert(NonMandatoryRegisterId::R8, Constant::from(generator()));
-        registers.insert(NonMandatoryRegisterId::R9, Constant::from(pp));
 
         let input_box = ErgoBox::new(
             erg_value,
@@ -273,14 +312,6 @@ mod tests {
         )
         .unwrap();
 
-        let mut constants = IndexMap::new();
-        constants.insert(0_u8, Constant::from(r.to_bytes().to_vec()));
-        constants.insert(1_u8, Constant::from(EcPoint::from(right)));
-        constants.insert(
-            2_u8,
-            Constant::from(signing_key.verifying_key().to_bytes().to_vec()),
-        );
-
         // Send all Ergs to miner fee
         let miner_output = ErgoBoxCandidate {
             value: erg_value,
@@ -290,7 +321,7 @@ mod tests {
             creation_height: 900001,
         };
         let outputs = TxIoVec::from_vec(vec![miner_output]).unwrap();
-        let unsigned_input = UnsignedInput::new(input_box.box_id(), ContextExtension { values: constants });
+        let unsigned_input = UnsignedInput::new(input_box.box_id(), ContextExtension::empty());
         let unsigned_tx =
             UnsignedTransaction::new(TxIoVec::from_vec(vec![unsigned_input]).unwrap(), None, outputs)
                 .unwrap();
